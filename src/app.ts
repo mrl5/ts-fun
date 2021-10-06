962477e86d4ab55787977afbe7921736a0cdd542
mrl5 <31549762+mrl5@users.noreply.github.com>
2021-10-06 22:31:58 +0200

    tsoa integration: generate swagger docs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express from 'express';
 import bodyParser from 'body-parser';
+import express, { Response as ExResponse, Request as ExRequest } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { RegisterRoutes } from '../build/routes';
+import spec from '../build/swagger.json';
 
 export const app = express();
 
@@ -11,5 +13,9 @@ app.use(
     }),
 );
 app.use(bodyParser.json());
+
+app.use('/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+    return res.send(swaggerUi.generateHTML(spec));
+});
 
 RegisterRoutes(app);
